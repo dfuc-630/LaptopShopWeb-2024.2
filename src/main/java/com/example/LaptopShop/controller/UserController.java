f062ed5c807fe1b79bb0eb135aef4e638fd9426b
Phuc <doandaiphuc0603@gmail.com>
2025-02-09 20:53:48 +0700

    setup construction

---
+++ src/main/java/com/example/LaptopShop/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.LaptopShop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.LaptopShop.service.UserService;
+
+// @Controller
+@RestController
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("")
+    public String getHomePage() {
+        return this.userService.handleHello();
+    }
+}
---
+++ src/main/java/com/example/LaptopShop/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.LaptopShop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello() {
+        return "hello from service of Doan Dai Phuc";
+    }
+}
