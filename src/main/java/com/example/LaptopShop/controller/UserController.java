d33a9baa6aefb6371eb6c9d39f419429d98b24b0
Phuc <doandaiphuc0603@gmail.com>
2025-02-13 09:04:12 +0700

    add data transfer test

--- src/main/java/com/example/LaptopShop/controller/UserController.java
+++ src/main/java/com/example/LaptopShop/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.example.LaptopShop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.LaptopShop.domain.User;
 import com.example.LaptopShop.service.UserService;
 
 @Controller
@@ -18,8 +21,23 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String test = this.userService.handleHello();
+        model.addAttribute("doanphuc", test);
+        model.addAttribute("dfuc", "from controller with model");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User doanphuc) {
+        System.out.println("run here" + doanphuc);
         return "hello";
     }
 
